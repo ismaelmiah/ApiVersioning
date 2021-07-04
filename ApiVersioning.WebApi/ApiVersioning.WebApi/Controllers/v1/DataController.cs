825ca5a5459d6fb35ba2412ea2946e4494f26a96
Ismael Hosen <ismail27.dec@gmail.com>
2021-07-04 13:08:50 +0600

    InitialProject

---
+++ ApiVersioning.WebApi/ApiVersioning.WebApi/Controllers/v1/DataController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVersioning.WebApi.Controllers.v1
+{
+// DEPRECATING an API Version
+    [ApiVersion("1.0", Deprecated = true)]
+    [Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Data From V1";
+        }
+    }
+}
---
+++ ApiVersioning.WebApi/ApiVersioning.WebApi/Controllers/v2/DataController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ApiVersioning.WebApi.Controllers.v2
+{
+    [ApiVersion("2.0")]
+    [Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Data From V2";
+        }
+    }
+}
